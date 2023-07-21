c4652b286b011c11db3af09cdef7c68c1ef4e886
elomd <elomdaa7444@gmail.com>
2023-07-21 22:24:47 +0300

    New Lesson Uploaded (Field & Constant-008)

---
+++ Field & Constant-008/Field & Constant-008/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    // «ClassModifier› -› public, internal (default)
+    //<ClassModifier> class <ClassName>
+    // {
+    //    class block
+    // }
+
+    public class Employee
+    {
+        // ‹AccessModifier> public, private, protected,
+        // Constant -> ‹AccessModifier> const <DataType> <ConstantName> = <value>;
+
+        public const double TAX = 0.03;
+
+        // ‹AccessModifier> public, private, protected,
+        // Fields -> ‹AccessModifier>  <DataType> <FieldsName> = <Initialvalue>
+
+        public string fName;
+        public string lName;
+        public double wage;
+        public double LoggedHours;
+
+
+    }
+}
---
+++ Field & Constant-008/Field & Constant-008/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    class program
+    {
+        static void Main (string[] args)
+        {
+            Employee[] emps = new Employee[2];
+            /* Object (istance) Syntax
+               Declaration <Type> <ObjectName>;
+               Assignment <ObjectName> = new  <Type>();
+               Initialization <Type> <ObjectName> <Type>(); */
+
+            Employee e1 = new Employee();
+            Console.Write("Enter Your First Name: ");
+            e1.fName = Console.ReadLine();
+
+            Console.Write("Enter Your Last Name: ");
+            e1.lName = Console.ReadLine();
+
+            Console.Write("Wage: ");
+            e1.wage = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("LoggedHours: ");
+            e1.LoggedHours = Convert.ToDouble(Console.ReadLine());
+
+            emps[0] = e1;
+
+            foreach (var emp in emps)
+            {
+                var netSalay = emp.wage * emp.LoggedHours - (emp.wage * emp.LoggedHours * Employee.TAX);
+                Console.WriteLine($"FirstName: {emp.fName}");
+                Console.WriteLine($"LastNamr: {emp.lName}");
+                Console.WriteLine($"Wage: {emp.wage}");
+                Console.WriteLine($"LoggedHours: {emp.LoggedHours}");
+                Console.WriteLine($"netSalay: {netSalay}");
+            }
+
+            //var netSalay = e1.wage * e1.LoggedHours - (e1.wage * e1.LoggedHours * Employee.TAX);
+
+           /* Console.WriteLine($"FirstName: {e1.fName}");
+            Console.WriteLine($"LastNamr: {e1.lName}");
+            Console.WriteLine($"Wage: {e1.wage}");
+            Console.WriteLine($"LoggedHours: {e1.LoggedHours}");
+            Console.WriteLine($"netSalay: {netSalay}"); 
+           */
+
+
+
+
+            Console.Read();
+        }
+    }
+}
