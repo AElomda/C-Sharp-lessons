fea829f93191368563d850be86c549a5d62b66ee
elomd <elomdaa7444@gmail.com>
2023-07-25 21:08:24 +0300

    Added New Lesson --011--

---
+++ Properties_011/Properties_011/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dollar dollar = new Dollar(1.99m);
+            dollar.SetAmount(1.99m);  //set
+            Console.WriteLine(dollar.Amount);  //get 
+            Console.WriteLine(dollar.IsZero);  
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Dollar
+    {
+
+        private decimal _amount;  //1.99, 120.99, 5.00 [1.009 => 1.01]
+        public int Amount { get; set; }
+        //public decimal Amount
+        //{
+        //    get
+        //    {
+        //        return this._amount;
+        //    }
+        //    private set
+        //    {
+        //        this._amount = value;
+        //    }
+        //}
+
+        public decimal getAmount()
+        {
+            return _amount;
+        }
+        public void setAmount(decimal value)
+        {
+            _amount = value;
+        }
+        public void SetAmount(decimal value)
+        {
+            Amount = value;
+        }
+        public bool IsZero => _amount == 0;
+
+        public decimal ConversionFactor { get; set; } = 1.99m;   
+
+        public Dollar(decimal amount)
+        {
+            this._amount = ProccessValue(amount);
+
+        }
+        private decimal ProccessValue(decimal value) => value <= 0 ? 0 : Math.Round(value, 2);
+
+    }
+}
