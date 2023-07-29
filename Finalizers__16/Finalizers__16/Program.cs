37845727935508432c4dcc618c596789b796ec8a
elomd <elomdaa7444@gmail.com>
2023-07-29 19:42:09 +0300

    Added New Lessons __15 , __16

---
+++ Finalizers__16/Finalizers__16/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Finalizers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MakeSomeGarabege();
+            Console.WriteLine($"Memory Used Before Collection {GC.GetTotalMemory(false):N}");
+            GC.Collect();
+            Console.WriteLine($"Memory Used After Collection: {GC.GetTotalMemory(true):N}");
+            Console.ReadKey();
+        }
+
+        static void MakeSomeGarabege()
+        {
+            Version v;
+            for (int i = 0; i < 1000; i++)
+            {
+                v = new Version();
+            }
+        }
+    }
+}
