e2a48434a65f7e93e0a64ceea577624d817ebe30
elomd <elomdaa7444@gmail.com>
2023-07-31 19:34:48 +0300

    Added New Lesson __18__

---
+++ Debugging__18/Debugging__18/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Debugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var amount = 1000;
+            var members = 4;
+ 
+            members = Reduce(members, 2);
+            var share = Distribute(amount, members);
+            Console.WriteLine(share);
+
+            Console.ReadLine();
+        }
+        static int Reduce(int members, int value)
+        {
+            return members -= value;
+        }
+        static int Distribute(int amount, int members)
+        {
+            try
+            {
+                return amount / members;
+            }
+            catch (Exception ex)
+            {
+                // In Case Exception is Thrown
+                Console.WriteLine($"Unexpected Error: {ex.Message}");
+            }
+            finally
+            {
+                // CLeanups
+            }
+            return 0;
+        }
+    }
+}
