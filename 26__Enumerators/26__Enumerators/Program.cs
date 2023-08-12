0e12629b8c87e90182932f9fa99242c6cb0d3f70
elomd <elomdaa7444@gmail.com>
2023-08-13 00:20:17 +0300

    Added New Lessons __26__27__

---
+++ 26__Enumerators/26__Enumerators/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections;
+
+namespace Enumerators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region First
+            //Employee e1 = new Employee { Id = 100, Name = "Issam A.", Salary = 1000m, Department = "IT" };
+            //Employee e2 = new Employee { Id = 100, Name = "Issam A.", Salary = 1000m, Department = "IT" };
+            //Employee e3 = e1;
+            ////Console.WriteLine(e1 == e2);        //references
+            ////Console.WriteLine(e3 == e1);        //references
+            //Console.WriteLine(e1.Equals(e2));   //content
+            //Console.WriteLine(e1 == e2);
+
+            //Console.WriteLine(7.GetHashCode()); 
+            #endregion
+            #region yield
+            //var ints = new FiveIntegars(1, 2, 3, 4, 5);
+            //foreach (var i in ints)
+            //{
+            //    Console.WriteLine(i);
+            //} 
+            #endregion
+
+            var temps = new List<Tempreture>();
+            Random rnd = new Random();
+            for (var i = 0; i < 100; i++)
+            {
+                temps.Add(new Tempreture(rnd.Next(-30, 50)));
+            }
+
+            temps.Sort();
+
+            foreach (var item in temps)
+            {
+                Console.WriteLine(item.Value);
+            }
+
+            Console.ReadKey();
+        }
+    }
+    #region First
+    //class Employee
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //    public decimal Salary { get; set; }
+    //    public string Department { get; set; }
+
+    //    public override bool Equals(object? obj)
+    //    {
+    //        if (obj == null || !(obj is Employee))
+    //            return false;
+
+    //        var emp = obj as Employee;
+
+    //        return this.Id == emp.Id
+    //            && this.Name == emp.Name
+    //            && this.Salary == emp.Salary
+    //            && this.Department == emp.Department;
+    //    }
+    //    public override int GetHashCode()
+    //    {
+    //        int hash = 13;
+    //        hash = (hash * 7) + Id.GetHashCode();
+    //        hash = (hash * 7) + Name.GetHashCode();
+    //        hash = (hash * 7) + Salary.GetHashCode();
+    //        hash = (hash * 7) + Department.GetHashCode();
+
+    //        return hash;
+    //    }
+    //    public static bool operator == (Employee lhs, Employee rhs) => lhs.Equals(rhs);
+    //    public static bool operator !=(Employee lhs, Employee rhs) => !lhs.Equals(rhs);
+
+    //} 
+    #endregion
+    #region yield
+    //class FiveIntegars : IEnumerable
+    //{
+    //    int[] _values;
+
+    //    public FiveIntegars(int n1, int n2, int n3, int n4, int n5)
+    //    {
+    //        _values = new[] { n1, n2, n3, n4, n5 }; 
+    //    }
+
+    //    public IEnumerator GetEnumerator() //=> new Enumerable(this); 
+    //    {
+    //        foreach (var item in _values)
+    //        {
+    //            yield return item;
+    //        }
+    //    } 
+    #endregion
+    #region We Can replace this big class by (yield)
+    //class Enumerable : IEnumerator
+    //{
+    //    int currentIndex = -1;
+    //    FiveIntegars _integers;
+    //    public Enumerable(FiveIntegars integars)
+    //    {
+    //        _integers = integars;
+    //    }
+
+    //    public object Current
+    //    {
+    //        get
+    //        {
+    //            if (currentIndex == -1)
+    //                throw new InvalidOperationException($"Enumeration not started");
+    //            if (currentIndex == _integers._values.Length)
+    //                throw new InvalidOperationException($"Enumeration has Ended");
+    //            return _integers._values[currentIndex];
+    //        }
+    //    }
+
+    //    public bool MoveNext()
+    //    {
+    //        if (currentIndex >= _integers._values.Length - 1)
+    //            return false;
+    //        return ++currentIndex < _integers._values.Length;
+    //    }
+
+    //    public void Reset() => currentIndex = -1;
+    //}  
+    #endregion
+    class Tempreture : IComparable
+    {
+        private int _value;
+        public Tempreture(int value)
+        {
+            _value = value;
+        }
+        public int Value => _value;
+
+        public int CompareTo(object obj)
+        {
+            if (obj is null)
+                return 1;
+            var temp = obj as Tempreture;
+            if (temp is null)
+                throw new ArgumentException("object is not a tempreture object");
+            return this._value.CompareTo(temp._value);
+        }
+    }
+}
---
+++ 27__XML/27__XML/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+namespace XML
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            do
+            {
+                Console.Write("First Name: ");
+                var fname = Console.ReadLine();
+
+                Console.Write("Last Name: ");
+                var lname = Console.ReadLine();
+
+                Console.Write("Hire Date: ");
+                DateTime? hireDate = null;
+                if(DateTime.TryParse(Console.ReadLine(), out DateTime hDate))
+                {
+                    hireDate = hDate;
+                }
+                var empId = Generator.GenerateId(fname, lname, hireDate);
+                var randomPassowrd = Generator.GenerateRandomPassword(8);
+                Console.WriteLine($"{{\n Id: {empId},\n FName: {fname},\n Lname: {lname},\n hire Date: {hireDate},\nPassword: {randomPassowrd}\n");
+            } while (1 == 1);
+            
+
+        }
+    }
+    /// <summary>
+    /// The Main Generator Class
+    /// </summary>
+    /// <remarks>
+    /// This class can generate employee Ids and passwords
+    /// </remarks>
+    public class Generator
+    {
+        /// <value> value of last Id sequence <value>   
+        public static int LastIdSequence { get; private set; } = 1;
+
+        /// <summary>
+        /// Generate Employee Id by processing <paramref name="fname"/>,<paramref name="lname"/> and <paramref name="hireData"/>
+        /// <list type="bullet">
+        /// <item>
+        /// <term>II</term>
+        /// <description>Employee Initial (First letter of <paramref name="lname"/> and first letter of <paramref name="fname"/>)</description>
+        /// </item>
+        /// </list>
+        /// </summary>
+        /// <param name="fname"></param>
+        /// <param name="lname"></param>
+        /// <param name="hireData"></param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException"></exception>
+        public static string GenerateId(string fname, string lname, DateTime? hireData)
+        {
+            // II YY MM DD 01
+            if (fname is null)
+                throw new InvalidOperationException($"{nameof(fname)} can not be null");
+            if (lname is null)
+                throw new InvalidOperationException($"{nameof(lname)} can not be null");
+            if (hireData is null)
+            {
+                hireData = DateTime.Now;
+            }
+            else
+            {
+                if(hireData.Value.Date < DateTime.Now.Date)
+                    throw new InvalidOperationException($"{nameof(hireData)} can not be in the past ");
+            }
+
+            var yy = hireData.Value.ToString("yy");
+            var mm = hireData.Value.ToString("MM");
+            var dd = hireData.Value.ToString("dd");
+
+            var code = $"{lname.ToUpper()[0]} {fname.ToUpper()[0]} {yy} {mm} {dd} {(LastIdSequence++).ToString().PadLeft(2, '0')}";
+            return code;
+        }
+
+        public static string GenerateRandomPassword(int lenght)
+        {
+            const string ValidScope = "adcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var result = "";
+            Random rnd = new Random();
+            while(0 < lenght--)
+            {
+                result += (ValidScope[rnd.Next(ValidScope.Length)]);
+            }
+            return result;
+        }
+    }
+}
